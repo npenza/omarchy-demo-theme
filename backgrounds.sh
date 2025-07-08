d8a7c4c2ec84bb3cda477a3790b9153ff8cbec79
Noah Penza <penzanoah@gmail.com>
2025-07-09 07:09:31 +1000

    update image link

--- backgrounds.sh
+++ backgrounds.sh
@@ -1,2 +1,2 @@
 mkdir -p "$BACKGROUNDS_DIR/omarchy-demo-theme"
-download_background_image "https://github.com/basecamp/omakub/raw/refs/heads/master/themes/nord/background.png" "omarchy-demo-theme/1-omarchy-demo-theme.png"
+download_background_image "https://github.com/npenza/omarchy-demo-theme/blob/main/background.png" "omarchy-demo-theme/1-omarchy-demo-theme.png"
