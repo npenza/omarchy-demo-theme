f0eaf6bf8698414589489a53ad129a4bd9d4b7a7
Noah Penza <penzanoah@gmail.com>
2025-07-09 21:12:50 +1000

    add additional background

--- backgrounds.sh
+++ backgrounds.sh
@@ -1,2 +1,3 @@
 mkdir -p "$BACKGROUNDS_DIR/omarchy-demo-theme"
-wget -O "$BACKGROUNDS_DIR/omarchy-demo-theme/1-omarchy-demo-theme.png" "https://raw.githubusercontent.com/npenza/omarchy-demo-theme/main/background.png"
+wget -O "$BACKGROUNDS_DIR/omarchy-demo-theme/1-omarchy-demo-theme.png" "https://raw.githubusercontent.com/npenza/omarchy-demo-theme/main/1-background.png"
+wget -O "$BACKGROUNDS_DIR/omarchy-demo-theme/1-omarchy-demo-theme.png" "https://raw.githubusercontent.com/npenza/omarchy-demo-theme/main/2-background.png"
