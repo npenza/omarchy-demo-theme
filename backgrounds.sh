19197b91e028cceee5e12907a2a8cd739e4accf6
Noah Penza <penzanoah@gmail.com>
2025-07-09 19:30:43 +1000

    update backgrounds.sh

--- backgrounds.sh
+++ backgrounds.sh
@@ -1,2 +1,2 @@
 mkdir -p "$BACKGROUNDS_DIR/omarchy-demo-theme"
-download_background_image "https://github.com/npenza/omarchy-demo-theme/blob/main/background.png" "omarchy-demo-theme/1-omarchy-demo-theme.png"
+wget -O "$BACKGROUNDS_DIR/omarchy-demo-theme/1-omarchy-demo-theme.png" "https://raw.githubusercontent.com/npenza/omarchy-demo-theme/main/background.png"
