0c112c57856f42efff26c398f05db5138d13657a
Noah Penza <penzanoah@gmail.com>
2025-07-13 14:57:08 +1000

    Refactor backgrounds

--- backgrounds.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p "$BACKGROUNDS_DIR/omarchy-demo-theme"
-wget -O "$BACKGROUNDS_DIR/omarchy-demo-theme/1-omarchy-demo-theme.png" "https://raw.githubusercontent.com/npenza/omarchy-demo-theme/main/1-background.png"
-wget -O "$BACKGROUNDS_DIR/omarchy-demo-theme/2-omarchy-demo-theme.png" "https://raw.githubusercontent.com/npenza/omarchy-demo-theme/main/2-background.png"
